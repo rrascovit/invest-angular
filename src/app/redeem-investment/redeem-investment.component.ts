a88cb78c4a54dfe816e2a67838eb1a5235d8cd91
Rafael Rascovit <rafaelrascovit@gmail.com>
2021-09-08 20:07:46 -0300

    Fix

--- src/app/redeem-investment/redeem-investment.component.ts
+++ src/app/redeem-investment/redeem-investment.component.ts
@@ -13,42 +13,74 @@ export class RedeemInvestmentComponent implements OnInit {
   investment: any = {};
   totalValue: number = 0;
   redeemShares: any[] = [];
+  isValid: boolean = true;
+  msg: any[] = [];
+  isValueValid: boolean = true;
 
   ngOnInit(): void {
     this.investment = history.state;
   }
 
-  open(isValid: boolean, msg: string) {
+  open(isValid: boolean, msg: string[]) {
     const modalRef = this.modalService.open(NgbdModalContent);
     modalRef.componentInstance.msg = msg;
     modalRef.componentInstance.isValid = isValid;
   }
 
   validateRedeem() {
+    this.isValid = true;
+    this.msg = [];
     if (this.redeemShares.length === 0 || this.totalValue === 0) {
-      const msg = 'Você deve indicar ao menos um valor a resgatar.';
-      return this.open(false, msg);
+      this.isValid = false;
+      this.msg.push({
+        id: 0,
+        text: 'Você deve indicar ao menos um valor a resgatar.',
+      });
+      return this.open(this.isValid, this.msg);
     }
 
-    this.redeemShares.every((item) => {
+    this.isValueValid = this.redeemShares.every(
+      (item) =>
+        item.value <=
+        (
+          (item.acao.percentual * this.investment.saldoTotalDisponivel) /
+          100
+        ).toFixed(2)
+    );
+
+    this.createMessage();
+
+    if (this.isValueValid) {
+      this.msg.push({
+        id: 0,
+        text: 'O valor solicitado estará em sua conta em até 5 dias úteis.',
+      });
+      this.open(this.isValid, this.msg);
+    } else {
+      this.isValid = false;
+      this.open(this.isValid, this.msg);
+    }
+  }
+
+  createMessage() {
+    this.redeemShares.forEach((item) => {
       const shareValue = (
         (item.acao.percentual * this.investment.saldoTotalDisponivel) /
         100
       ).toFixed(2);
       if (item.value > shareValue) {
-        const msg = `Valor invalida da ação ${item.acao.nome}, o máximo disponível é ${shareValue}`;
-        return this.open(false, msg);
+        this.msg.push({
+          id: item.acao.id,
+          text: `Valor invalido da ação ${item.acao.nome}, o máximo disponível é ${shareValue}`,
+        });
       }
-      const msg = 'O valor solicitado estará em sua conta em até 5 dias úteis.';
-      return this.open(true, msg);
     });
   }
 
   onKey(acao: any, event: any) {
     const value = +event.target.value
       .replace(/[^0-9.,]+/, '')
-      .replace(/\s/g, '')
-      .replace('.', '')
+      .replace(/\./g, '')
       .replace(',', '.');
 
     const index = this.redeemShares.findIndex(
